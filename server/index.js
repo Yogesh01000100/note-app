0d402f770fcd46f7582206010ef477355d99dad6
D.Yogesh <yogeshone678@gmail.com>
2024-07-19 04:56:44 +0530

    fix: server file position in root
    Signed-off-by: D.Yogesh <yogeshone678@gmail.com>

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ mongoose.connect(process.env.DB_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.use('/api/users', require('./routes/authRoutes'));
-app.use('/api/notes', require('./routes/noteRoutes'));
+app.use('/api/users', require('./src/routes/authRoutes'));
+app.use('/api/notes', require('./src/routes/noteRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
